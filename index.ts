e233a91102363307637ca8080d263424b0d9521d
AlfanCodes <alfancodes@gmail.com>
2020-05-20 02:20:40 +0700

    update codes add simple rest API add quotes

---
+++ index.ts
@@ -0,0 +1,28 @@
+// Add DenoTrain module
+import { Application, Router } from "https://deno.land/x/denotrain@v0.4.4/mod.ts";
+// Load quotes
+import { quotes } from "./quotes.ts";
+
+// Create a new application
+const app = new Application({ hostname: "127.0.0.1", port: 3000 });
+// Optional: Generate router and hook routes to it
+const router = new Router();
+
+// Hook up the router on "/api"
+app.use("/api", router);
+
+app.get("/", context => {
+    return "Hello from AlfanCodes";
+});
+
+// url will be at http://127.0.0.1:3000/api/quotes
+router.get("/quotes", context => {
+    return { quotes: quotes };
+});
+
+// Single quote, url will be at http://127.0.0.1:3000/api/quote
+router.get("/quote", context => {
+    return { quote: quotes[Math.floor(Math.random() * quotes.length)] };
+});
+
+await app.run();
